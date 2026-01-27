b8d3ffb9cc0e0777b04ff20ef201a2da6463b28f
Jonghwa <whdghk3471@gmail.com>
2026-01-27 19:56:21 +0900

    #3.3 Movies Screen part Two

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,6 +1,9 @@
+import { makeImgPath } from "@/utils";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Dimensions } from "react-native";
-import Swiper from "react-native-web-swiper";
+import { BlurView } from "expo-blur";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, Dimensions, StyleSheet } from "react-native";
+import Swiper from "react-native-swiper";
 import { styled } from "styled-components/native";
 
 const API_KEY = "0e7caaa451bd63724c4d4ff302137c3e";
@@ -13,40 +16,62 @@ const View = styled.View`
   flex: 1;
 `;
 
+const Loader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.mainBgColor};
+`;
+
+const BgImg = styled.Image``;
+
+const Title = styled.Text``;
+
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 export default function Movies({
   navigation,
 }: NativeStackScreenProps<any, "Movies">) {
-  const getNowPlaying = () => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}language=en-US&page=1&region=KR`,
-    );
+  const [loading, setLoading] = useState(true);
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const getNowPlaying = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`,
+      )
+    ).json();
+    setNowPlaying(results);
+    setLoading(false);
   };
-  return (
+  useEffect(() => {
+    getNowPlaying();
+  }, []);
+  return loading ? (
+    <Loader>
+      <ActivityIndicator />
+    </Loader>
+  ) : (
     <Container>
       <Swiper
         loop
-        timeout={3.5}
-        springConfig={{
-          stiffness: 120,
-          damping: 20,
-          mass: 1,
-        }}
-        controlsProps={{
-          dotsTouchable: true,
-          prevPos: false,
-          nextPos: false,
-          dotActiveStyle: {
-            backgroundColor: "black",
-          },
-        }}
+        autoplay
+        autoplayTimeout={3.5}
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+        paginationStyle={{
+          bottom: 10,
+        }}
       >
-        <View style={{ backgroundColor: "red" }}></View>
-        <View style={{ backgroundColor: "blue" }}></View>
-        <View style={{ backgroundColor: "red" }}></View>
-        <View style={{ backgroundColor: "blue" }}></View>
+        {nowPlaying.map((movie) => (
+          <View key={movie.id}>
+            <BgImg
+              style={StyleSheet.absoluteFill}
+              source={{ uri: makeImgPath(movie.backdrop_path) }}
+            />
+            <BlurView intensity={80} style={StyleSheet.absoluteFill}>
+              <Title>{movie.original_title}</Title>
+            </BlurView>
+          </View>
+        ))}
       </Swiper>
     </Container>
   );
