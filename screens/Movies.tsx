607acf8954d4c0dcaef9719ab66fa714f3b33c5e
Jonghwa <whdghk3471@gmail.com>
2026-01-30 19:15:11 +0900

    #3.7 Coming Soon

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -25,14 +25,44 @@ const ListTitle = styled.Text`
   font-size: 18px;
   font-weight: 600;
   margin-left: 20px;
+  margin-bottom: 10px;
+`;
+
+const ListContainer = styled.View`
+  margin-bottom: 30px;
 `;
 
 const Movie = styled.View`
   margin-right: 20px;
 `;
 
-const TrendingScroll = styled.ScrollView`
-  margin-top: 10px;
+const Title = styled.Text`
+  color: white;
+  font-weight: 600;
+`;
+
+const TrendingScroll = styled.ScrollView``;
+
+const HMovie = styled.View`
+  padding: 0 20px;
+  flex-direction: row;
+  gap: 15px;
+  margin-bottom: 20px;
+`;
+
+const HColumn = styled.View`
+  width: 80%;
+`;
+
+const OverView = styled.Text`
+  color: rgba(255, 255, 255, 0.8);
+  width: 80%;
+`;
+
+const Release = styled.Text`
+  color: white;
+  font-size: 12px;
+  margin: 10px 0;
 `;
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
@@ -113,17 +143,36 @@ export default function Movies({
         ))}
       </Swiper>
       <ListTitle>지금 뜨는 영화</ListTitle>
-      <TrendingScroll
-        horizontal
-        showsHorizontalScrollIndicator={true}
-        contentContainerStyle={{ paddingLeft: 20 }}
-      >
-        {trending.map((movie) => (
-          <Movie key={movie.id}>
-            <Poster path={movie.poster_path} />
-          </Movie>
-        ))}
-      </TrendingScroll>
+      <ListContainer>
+        <TrendingScroll
+          horizontal
+          showsHorizontalScrollIndicator={true}
+          contentContainerStyle={{ paddingLeft: 20 }}
+        >
+          {trending.map((movie) => (
+            <Movie key={movie.id}>
+              <Poster path={movie.poster_path} />
+            </Movie>
+          ))}
+        </TrendingScroll>
+      </ListContainer>
+      <ListTitle>곧 개봉</ListTitle>
+      {upcoming.map((movie) => (
+        <HMovie key={movie.id}>
+          <Poster path={movie.poster_path} />
+          <HColumn>
+            <Title>{movie.title}</Title>
+            <Release>
+              {new Date(movie.release_date).toLocaleDateString("ko", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}
+            </Release>
+            <OverView>{movie.overview.slice(0, 140)}...</OverView>
+          </HColumn>
+        </HMovie>
+      ))}
     </Container>
   );
 }
